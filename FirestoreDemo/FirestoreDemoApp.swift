2922d676a40a9b43d44fb0d13ca95e4f20cb1c40
Chuck Belcher <cfbelcher@pateogroup.com>
2023-08-04 15:04:59 -0400

    Initial Commit

---
+++ FirestoreDemo/FirestoreDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirestoreDemoApp.swift
+//  FirestoreDemo
+//
+//  Created by Chuck Belcher on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FirestoreDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
